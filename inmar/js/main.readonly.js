2e4b6c489ad19f999d0e947f9e58733ba69d7875
Dima Bellavin <izume@inbox.ru>
2020-07-14 10:45:09 +0300

    inmar

---
+++ inmar/js/main.readonly.js
@@ -0,0 +1,209 @@
+import {forEachPolyfill} from './utils/polyfill-foreach';
+import {initIe11Download} from './utils/init-ie11-download';
+import {menu} from './utils/menu'
+
+// Utils
+// ---------------------------------
+forEachPolyfill();
+initIe11Download();
+
+menu(`.top-menu__left-menu`, `.top-menu__left-menu-burger`, `.top-menu__left-menu-overlay`);
+
+
+// Modules
+// ---------------------------------
+
+const ie11Download = (el) => {
+  if (el.href === ``) {
+    throw Error(`The element has no href value.`);
+  }
+
+  let filename = el.getAttribute(`download`);
+  if (filename === null || filename === ``) {
+    const tmp = el.href.split(`/`);
+    filename = tmp[tmp.length - 1];
+  }
+
+  el.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    const xhr = new XMLHttpRequest();
+    xhr.onloadstart = () => {
+      xhr.responseType = `blob`;
+    };
+    xhr.onload = () => {
+      navigator.msSaveOrOpenBlob(xhr.response, filename);
+    };
+    xhr.open(`GET`, el.href, true);
+    xhr.send();
+  });
+};
+
+const downloadLinks = document.querySelectorAll(`a[download]`);
+
+const initIe11Download = () => {
+  if (window.navigator.msSaveBlob) {
+    if (downloadLinks.length) {
+      downloadLinks.forEach((el) => {
+        ie11Download(el);
+      });
+    }
+  }
+};
+
+export {initIe11Download};
+
+export const menu = (menuClass, burgerClass, overlayClass) => {
+  const menuElem = document.querySelector(menuClass);
+  const brugerElem = menuElem.querySelector(burgerClass);
+  const overlayElem = menuElem.querySelector(overlayClass);
+
+  const escKeyDownHandler = (evt) => {
+    if (evt.key === `Escape` || evt.key === `Esc`) {
+      closeMenuHandler();
+    }
+  };
+
+  const openMenuHandler = () => {
+    menuElem.classList.add(`active`);
+    brugerElem.querySelector(`.burger`).classList.add(`active`);
+    document.addEventListener(`keydown`, escKeyDownHandler);
+  }
+
+  const closeMenuHandler = () => {
+    menuElem.classList.remove(`active`);
+    brugerElem.querySelector(`.burger`).classList.remove(`active`);
+    document.removeEventListener(`keydown`, escKeyDownHandler);
+  }
+
+  brugerElem.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    const isOpen = menuElem.classList.contains(`active`);
+
+    if (!isOpen) {
+      openMenuHandler();
+    } else {
+      closeMenuHandler();
+    }
+  });
+
+  overlayElem.addEventListener(`click`, () => {
+    closeMenuHandler();
+  })
+}
+
+const forEachPolyfill = () => {
+  if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+  }
+};
+
+export {forEachPolyfill};
+
+const body = document.querySelector(`body`);
+
+const getScrollbarWidth = () => {
+  const outer = document.createElement(`div`);
+  outer.style.visibility = `hidden`;
+  outer.style.overflow = `scroll`;
+  outer.style.msOverflowStyle = `scrollbar`;
+  body.appendChild(outer);
+  const inner = document.createElement(`div`);
+  outer.appendChild(inner);
+  const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+  outer.parentNode.removeChild(outer);
+  if (outer.offsetWidth !== inner.offsetWidth) {
+    return scrollbarWidth;
+  }
+};
+
+const getBodyScrollTop = () => {
+  return (
+    self.pageYOffset ||
+    (document.documentElement && document.documentElement.ScrollTop) ||
+    (body && body.scrollTop)
+  );
+};
+
+const disableScrolling = () => {
+  const scrollWidth = getScrollbarWidth();
+  body.setAttribute(`style`, `padding-right: ` + scrollWidth + `px;`);
+  body.dataset.scrollY = `${getBodyScrollTop()}`;
+  body.style.top = `-${body.dataset.scrollY}px`;
+  body.classList.add(`scroll-lock`);
+};
+
+const enableScrolling = () =>{
+  body.removeAttribute(`style`);
+  body.classList.remove(`scroll-lock`);
+  window.scrollTo(0, +body.dataset.scrollY);
+};
+
+export {enableScrolling, disableScrolling};
+
+export const slideUp = (target, duration) => {
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = duration + 'ms';
+  target.style.boxSizing = 'border-box';
+  target.style.height = target.offsetHeight + 'px';
+
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.style.overflow = 'hidden';
+
+  window.setTimeout( () => {
+    target.style.display = 'none'; /* [8] */
+    target.style.removeProperty('height'); /* [9] */
+    target.style.removeProperty('padding-top');  /* [10.1] */
+    target.style.removeProperty('padding-bottom');  /* [10.2] */
+    target.style.removeProperty('margin-top');  /* [11.1] */
+    target.style.removeProperty('margin-bottom');  /* [11.2] */
+    target.style.removeProperty('overflow');  /* [12] */
+    target.style.removeProperty('transition-duration');  /* [13.1] */
+    target.style.removeProperty('transition-property');  /* [13.2] */
+  }, duration);
+}
+
+
+export const slideDown = (target, duration) => {
+  target.style.removeProperty('display');
+  let display = window.getComputedStyle(target).display;
+  if (display === 'none') {
+    display = 'block';
+  }
+  target.style.display = display;
+
+  let height = target.offsetHeight;
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.style.overflow = 'hidden';
+
+  target.style.boxSizing = 'border-box';
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = duration + 'ms';
+  target.style.height = height + 'px';
+  target.style.removeProperty('padding-top');
+  target.style.removeProperty('padding-bottom');
+  target.style.removeProperty('margin-top');
+  target.style.removeProperty('margin-bottom');
+
+  window.setTimeout( () => {
+    target.style.removeProperty('height'); /* [13] */
+    target.style.removeProperty('overflow'); /* [14] */
+    target.style.removeProperty('transition-duration'); /* [15.1] */
+    target.style.removeProperty('transition-property'); /* [15.2] */
+  }, duration);
+}
+
+export const slideToggle = (target, duration = 500) => {
+  if (window.getComputedStyle(target).display === 'none') {
+    return slideDown(target, duration);
+  } else {
+    return slideUp(target, duration);
+  }
+}
