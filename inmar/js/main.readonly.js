4db187c52afb5b1a5b0d677fbe62069075a77460
Dima Bellavin <izume@inbox.ru>
2020-07-26 02:00:06 +0300

    inmar

--- inmar/js/main.readonly.js
+++ inmar/js/main.readonly.js
@@ -284,13 +284,46 @@ export const homepage = () => {
 }
 
 export const productInner = () => {
-  var mySwiper = new Swiper('.product-inner .swiper-container', {
+  var mySwiper = new Swiper(`.product-inner .swiper-container`, {
     loop: true,
-    slidesPerView: 4,
-
+    slidesPerView: 1,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: `.swiper-button-next`,
+      prevEl: `.swiper-button-prev`,
     },
-  })
+    breakpoints: {
+
+      500: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      }
+    }
+  });
+
+
+
+  const setPrice = () => {
+    const formElem = document.querySelector(`.js-product-inner-form`);
+    const radioElems = formElem.querySelectorAll(`.js-product-inner-radio`);
+    const priceElem = formElem.querySelector(`.js-product-inner-price`);
+
+    const updatePrice = () => {
+      const radioElem = [...radioElems].filter((elem) => elem.checked)[0];
+      priceElem.textContent = radioElem.value.toLocaleString('ru-RU') + ` Р`;
+    }
+
+    if (formElem) {
+      updatePrice();
+      formElem.addEventListener(`change`, () => {
+        updatePrice();
+      });
+    }
+  }
+
+  setPrice();
 }
