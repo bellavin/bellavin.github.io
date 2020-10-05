4243047d5b071273499b37bcc45cb1ce04b4cab1
Dima Bellavin <izume@inbox.ru>
2020-10-05 14:41:55 +0300

    inmar

--- inmar/js/main.readonly.js
+++ inmar/js/main.readonly.js
@@ -502,6 +502,39 @@ export const portfolioInner = () => {
 
 export const productInner = () => {
   const productInnerElem = document.querySelector(`.product-inner`);
+  const dataProductInnerInput = document.querySelectorAll(`[data-product-inner-input]`);
+  const dataProductInnerPic = document.querySelectorAll(`[data-product-inner-pic]`);
+
+
+  if (dataProductInnerInput && dataProductInnerPic) {
+
+    const removeActiveClass = () => {
+      dataProductInnerPic.forEach((elem) => {
+        if (elem.classList.contains(`active`)) {
+          elem.classList.remove(`active`);
+        }
+      });
+    }
+
+    removeActiveClass();
+
+    dataProductInnerInput.forEach((elem) => {
+      const elemId = elem.dataset.productInnerInput;
+
+      const setActiveClass = () => {
+        if (elem.checked) {
+          dataProductInnerPic[elemId-1].classList.add(`active`);
+        }
+      }
+
+      setActiveClass();
+
+      elem.addEventListener(`change`, () => {
+        removeActiveClass();
+        setActiveClass();
+      });
+    });
+  }
 
   if (productInnerElem) {
     var mySwiper = new Swiper(`.product-inner .swiper-container`, {
